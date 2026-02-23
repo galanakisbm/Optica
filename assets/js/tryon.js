ab9315bdd0a14993a7b47da5115ed132fdfdeab4
galanakis vasileios marios <130051902+galanakisbm@users.noreply.github.com>
2026-02-23 18:58:02 +0200

    Add main JavaScript try-on logic

---
+++ assets/js/tryon.js
@@ -0,0 +1,9 @@
+// Main JavaScript try-on logic
+
+function startTryOn() {
+    // Initialize try-on process
+    console.log('Try-on process started');
+    // Logic for try-on can be implemented here
+}
+
+startTryOn();
